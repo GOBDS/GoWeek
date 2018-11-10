2f8c1ec7cf3e13d4c01173cdc91450a7a365991e
GOBDS <gabriel_bernardo@outlook.com>
2018-11-10 14:51:48 -0200

    Backend in nodeJs

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+mongoose.connect('mongodb://goweek:goweek123@ds157853.mlab.com:57853/gobs_goweek', {
+    useNewUrlParser: true
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    return next();
+});
+app.use(cors());
+app.use(express.json())
+app.use(require('./routes'));
+
+server.listen(3000, () => {
+    console.log('Server Started on port 3000');
+});
+
